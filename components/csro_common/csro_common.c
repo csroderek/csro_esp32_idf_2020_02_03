15e4027183412adb20fe76d6d48562cdcf5ceef2
dereklitaoo <dereklitao@gmail.com>
2020-02-03 19:28:34 +0800

    init csro_main

--- components/csro_common/csro_common.c
+++ components/csro_common/csro_common.c
@@ -1,6 +1,26 @@
 #include "csro_common.h"
 
+csro_system sysinfo;
+csro_mqtt mqttinfo;
+esp_mqtt_client_handle_t mqttclient;
+
 void csro_main(void)
 {
-    printf("Starting csro main app......\r\n");
+    nvs_handle handle;
+    nvs_flash_init();
+    nvs_open("system", NVS_READWRITE, &handle);
+    nvs_get_u32(handle, "power_cnt", &sysinfo.power_cnt);
+    nvs_set_u32(handle, "power_cnt", (sysinfo.power_cnt + 1));
+    nvs_get_u8(handle, "router_flag", &sysinfo.router_flag);
+    nvs_commit(handle);
+    nvs_close(handle);
+    printf("Power count %d.\r\n", sysinfo.power_cnt);
+    if (sysinfo.router_flag == 1)
+    {
+        printf("starting mqtt......\r\n");
+    }
+    else
+    {
+        printf("starting smart......\r\n");
+    }
 }
--- components/csro_common/include/csro_common.h
+++ components/csro_common/include/csro_common.h
@@ -7,17 +7,64 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "nvs_flash.h"
+#include "mqtt_client.h"
 
 #define CONFIG_IDF_TARGET_ESP32
 
-#ifdef CONFIG_IDF_TARGET_ESP32
-#define CHIP_NAME "ESP32"
-#endif
+//#define USE_CLOUD_SERVER
 
-#ifdef CONFIG_IDF_TARGET_ESP32S2BETA
-#define CHIP_NAME "ESP32-S2 Beta"
-#endif
+#define MANUFACTURER "NANJING CSRO CO.,LTD."
+#define SOFT_VERSION "2020-02-03"
 
+#define MOTOR_CSRO_3K2R
+/*
+#define MOTOR_CSRO_3K2R
+#define DLIGHT_CSRO_3K3SCR
+*/
+
+typedef struct
+{
+    uint8_t restore_flag;
+    uint8_t router_flag;
+    char router_ssid[50];
+    char router_pass[50];
+    uint8_t mac[6];
+    char mac_str[20];
+    uint8_t ip[4];
+    char ip_str[20];
+    char host_name[20];
+    char dev_type[20];
+    uint32_t power_cnt;
+    uint32_t time_run;
+} csro_system;
+
+typedef struct
+{
+    char id[50];
+    char name[50];
+    char pass[50];
+    char sub_topic[100];
+    char pub_topic[100];
+    char lwt_topic[100];
+    char content[1000];
+    char broker[50];
+    char uri[50];
+    char prefix[50];
+} csro_mqtt;
+
+extern csro_system sysinfo;
+extern csro_mqtt mqttinfo;
+extern esp_mqtt_client_handle_t mqttclient;
+
+//csro_common.c
 void csro_main(void);
+void csro_reset_router(void);
+
+//csro_smart.c
+void csro_smart_task(void);
+
+//csro_mqtt.c
+void csro_mqtt_task(void);
 
 #endif
