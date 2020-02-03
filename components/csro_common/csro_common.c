6cf2e37c8f375a9e5f7852f439d1fd45fc1b5026
dereklitaoo <dereklitao@gmail.com>
2020-02-03 18:51:07 +0800

    init commit

---
+++ components/csro_common/csro_common.c
@@ -0,0 +1,6 @@
+#include "csro_common.h"
+
+void csro_main(void)
+{
+    printf("Starting csro main app......\r\n");
+}
---
+++ components/csro_common/include/csro_common.h
@@ -0,0 +1,23 @@
+#ifndef _CSRO_COMMON_H
+#define _CSRO_COMMON_H
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#define CONFIG_IDF_TARGET_ESP32
+
+#ifdef CONFIG_IDF_TARGET_ESP32
+#define CHIP_NAME "ESP32"
+#endif
+
+#ifdef CONFIG_IDF_TARGET_ESP32S2BETA
+#define CHIP_NAME "ESP32-S2 Beta"
+#endif
+
+void csro_main(void);
+
+#endif
